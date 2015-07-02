46cac40a69e2b03ddd321e43aa15135ebb2b5d86
Basil Siddiqui <basil.siddiqui@gmail.com>
2015-07-02 16:13:41 -0700

    fix(header): fix ui header bug for browser

--- www/js/app.js
+++ www/js/app.js
@@ -82,14 +82,14 @@ angular.module('earlybird', ['ionic', 'ngCookies', 'earlybird.services', 'earlyb
     }
   })
 
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+  // $ionicPlatform.ready(function() {
+  //   // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+  //   // for form inputs)
+  //   if(window.cordova && window.cordova.plugins.Keyboard) {
+  //     cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  //   }
+  //   if(window.StatusBar) {
+  //     StatusBar.styleDefault();
+  //   }
+  // });
 })
